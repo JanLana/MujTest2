ae408844545f35e7a82b50cfd83f1ca42dced925
Jan Lana <jan.lana@gyarab.cz>
2025-01-13 11:06:34 +0100

    oprava

--- src/main/java/org/example/MyMin.java
+++ src/main/java/org/example/MyMin.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class MyMin {
     public static int min(int x, int y) {
-        return x < y ? x : x;
+        return x < y ? x : y;
     }
 
     public static void main(String[] argv) {
         Scanner sc = new Scanner(System.in);
-        var vysledek = 10;
+        var vysledek = sc.nextInt();
         System.out.println("Nejmensi cislo ze vsech je: " + vysledek);
     }
 }
